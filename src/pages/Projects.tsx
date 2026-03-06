e17c2efb71c5f0fa30420444248f6dc16ce9c9db
Naman <namansingla830@gmail.com>
2026-03-06 13:10:16 +0530

    project set up

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import type { Project } from "../types";
+import { Loader2Icon } from "lucide-react";
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
+  const { projectId } = useParams();
+  const navigate = useNavigate();
 
-export default Projects
+  const [project, setProject] = useState<Project | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const [isGenerating, setIsGenerating] = useState(true);
+  const [device, setDevice] = useState<"phone" | "tablet" | "desktop">("desktop");
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
+
+  const fetchProject = async () => {
+
+  };
+
+  useEffect(() => {
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-[80vh]">
+        <Loader2Icon className="animate-spin size-7 text-primary" />
+      </div>
+    );
+  }
+
+  return <div>Projects</div>;
+};
+
+export default Projects;
