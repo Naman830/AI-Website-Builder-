f24ca13a41fa95f90d7fb52e898935f4bb2bbb57
Naman <namansingla830@gmail.com>
2026-03-09 21:58:55 +0530

    route added in server.ts

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,14 +16,14 @@ const corsOptions = {
 
 // Middleware
 app.use(cors(corsOptions));
-
 app.all("/api/auth/{*any}", toNodeHandler(auth));
+app.use(express.json({limit: '50mb'}));
 
-app.use(express.json());
-
+// ROUTE
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is Live!");
 });
+app.use('/api/user', userRouter)
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
