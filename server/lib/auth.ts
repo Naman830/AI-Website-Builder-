1514b17dc91e73b5d048f2843c0f942fc7978e6a
Naman <namansingla830@gmail.com>
2026-03-09 15:58:39 +0530

    bug fixed fetch problem

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -1,31 +1,31 @@
-import 'dotenv/config'
+import "dotenv/config";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma.js";
 
-const trustedOrigins = process.env.TRUSTED_ORIGINS?.split(',') || []
+const trustedOrigins = process.env.TRUSTED_ORIGINS?.split(",") || [];
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", 
-    }),
-      emailAndPassword: { 
-    enabled: true, 
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  emailAndPassword: {
+    enabled: true,
   },
   trustedOrigins,
   baseURL: process.env.BETTER_AUTH_URL!,
   secret: process.env.BETTER_AUTH_SECRET!,
   advanced: {
     cookies: {
-        session_token: {
-            name: 'auth_session',
-            attributes: {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'none',
-                path: '/'
-            }
+      session_token: {
+        name: "auth_session",
+        attributes: {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'none',
+            path: '/',
         }
-    }
-  } 
+      },
+    },
+  },
 });
--- server/server.ts
+++ server/server.ts
@@ -1,28 +1,29 @@
 import "dotenv/config";
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 import cors from "cors";
+
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+const corsOptions = {
+  origin: process.env.TRUSTED_ORIGINS?.split(",") || [],
+  credentials: true,
+};
 
 // Middleware
+app.use(cors(corsOptions));
 
-const port = process.env.PORT || 3000;
-const corsOption = {
-    origin: process.env.TRUSTED_ORIGINS?.split(',') || [],
-    credentials: true,
-}
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 
-app.use(cors(corsOption))
-app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.use(express.json());
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Server is Live!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is Live!");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
