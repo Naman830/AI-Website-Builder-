bfaadabe210e4d50c17c11f38744f342893e04e9
Naman <namansingla830@gmail.com>
2026-03-09 20:21:04 +0530

    created middleware for userId

---
+++ server/controllers/userController.ts
@@ -0,0 +1,10 @@
+import {Request, Response} from 'express'
+
+// GET USER CRREDITS
+export const getUserCredits = async(req: Request, res: Response) => {
+    try {
+        const userId = req.userId;
+    } catch (error) {
+        
+    }
+}
