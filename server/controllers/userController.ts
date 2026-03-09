342a5193b14ada512066cf9e9fed553a4041e333
Naman <namansingla830@gmail.com>
2026-03-09 20:24:38 +0530

    userController created for credits

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,10 +1,24 @@
-import {Request, Response} from 'express'
+import { Request, Response } from "express";
+import prisma from "../lib/prisma.js";
 
 // GET USER CRREDITS
-export const getUserCredits = async(req: Request, res: Response) => {
-    try {
-        const userId = req.userId;
-    } catch (error) {
-        
+export const getUserCredits = async (req: Request, res: Response) => {
+  try {
+    const userId = req.userId;
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized User" });
     }
-}
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    res.json({
+      credits: user?.credits,
+    });
+  } catch (error: any) {
+    console.log();
+    console.log(error.code || error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
