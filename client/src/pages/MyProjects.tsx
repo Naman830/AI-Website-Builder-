50b3ae01c6529c67870a27e43ae978cfc335ebd0
Naman <namansingla830@gmail.com>
2026-03-12 17:13:26 +0530

    delete project working well

--- client/src/pages/MyProjects.tsx
+++ client/src/pages/MyProjects.tsx
@@ -4,30 +4,48 @@ import { Loader2Icon, PlusIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import api from "@/configs/axios";
 import { toast } from "sonner";
-
+import { authClient } from "@/lib/auth-client";
 
 const MyProjects = () => {
+  const { data: session, isPending } = authClient.useSession();
   const [loading, setLoading] = useState(true);
   const [projects, setProjects] = useState<Project[]>([]);
   const navigate = useNavigate();
 
   const fetchProject = async () => {
     try {
-      const { data } = await api.get('/api/user/projects')
-      setProjects(data.projects)
-setLoading(false)
+      const { data } = await api.get("/api/user/projects");
+      setProjects(data.projects);
+      setLoading(false);
     } catch (error: any) {
       console.log(error);
-           toast.error(error?.response?.data?.message || error.message);
-
+      toast.error(error?.response?.data?.message || error.message);
     }
   };
 
-  const deleteProject = async (projectId: string) => {};
+  const deleteProject = async (projectId: string) => {
+    try {
+      const confirm = window.confirm(
+        "Are u sure you want to delete this project",
+      );
+      if (!confirm) return;
+      const { data } = await api.delete(`/api/project/${projectId}`);
+      toast.success(data.message);
+      fetchProject();
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error?.response?.data?.message || error.message);
+    }
+  };
 
   useEffect(() => {
-    fetchProject();
-  }, []);
+    if (session?.user && !isPending) {
+      fetchProject();
+    } else if (!isPending && !session?.user) {
+      navigate("/");
+      toast("Please login to view your project");
+    }
+  }, [session?.user]);
 
   return (
     <>
