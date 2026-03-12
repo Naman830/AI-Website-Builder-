cbfc5ae6029bd77bfbef5b2d71947014e31f6760
Naman <namansingla830@gmail.com>
2026-03-12 23:49:27 +0530

    community projects working

--- client/src/pages/Community.tsx
+++ client/src/pages/Community.tsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import type { Project } from "../types";
 import { Loader2Icon, PlusIcon, TrashIcon } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
-import { dummyProjects } from "../assets/assets";
+import api from "@/configs/axios";
+import { toast } from "sonner";
 
 const Community = () => {
   const [loading, setLoading] = useState(true);
@@ -10,11 +11,14 @@ const Community = () => {
   const navigate = useNavigate();
 
   const fetchProject = async () => {
-    setProjects(dummyProjects);
-    // simulate loading
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
+    try {
+      const {data} = await api.get('/api/project/published ')
+      setProjects(data.projects)
+      setLoading(false)
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error?.response?.data?.message || error.message);
+    }
   };
 
   useEffect(() => {
